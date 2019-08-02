8cb2b8f3cacafd179bf4ce0f628615d391cdd009
Olof S <olof.spango@gmail.com>
2019-08-03 00:25:10 +0200

    Fixed bug w/ env. var

--- src/models/user.js
+++ src/models/user.js
@@ -80,7 +80,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function() {
     const user = this
-const token = jwt.sign({_id:user.id.toString()}, env.params.JWT_SECRET)
+const token = jwt.sign({_id:user.id.toString()}, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({token})
     await user.save()
